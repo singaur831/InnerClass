c416568ac54e10924071b45d8b101e19b79b26b6
singaur831 <70050421+singaur831@users.noreply.github.com>
2021-05-17 11:02:20 +0530

    Update TestMemberOuter1.java
    Added the comment

--- src/InnerClass2/TestMemberOuter1.java
+++ src/InnerClass2/TestMemberOuter1.java
@@ -3,11 +3,11 @@ package InnerClass2;
 class TestMemberOuter1{  
 	 private int data=30;  
 	 class Inner{  
-	  void msg(){System.out.println("data is "+data);}  
+	  void msg(){System.out.println("data is "+data);} //Data will be reflected here in the meanwhile. 
 	 }  
 	 public static void main(String args[]){  
 	  TestMemberOuter1 obj=new TestMemberOuter1();  
-	  TestMemberOuter1.Inner in=obj.new Inner();  
+	  TestMemberOuter1.Inner in=obj.new Inner();  //check out the data 
 	  in.msg();  
 	 }  
 	}  
